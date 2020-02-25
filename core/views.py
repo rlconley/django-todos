3f36e9de0d04250f55e0f0a99b490071df43c865
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2020-02-25 01:05:50 -0500

    add todo model

--- core/views.py
+++ core/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from .models import Todo
 import data
 
 # Create your views here.
 def todos_list(request):
-    todos = data.TODOS
+    todos = Todo.objects.all()
     return render(request, 'core/todos_list.html', {'todos': todos})
 
 def todos_detail(request, pk):
-    todo = data.TODOS[str(pk)]
+    todo = Todo.objects.get(pk=pk)
     return render(request, 'core/todos_detail.html', {'todo': todo})
